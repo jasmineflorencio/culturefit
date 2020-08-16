e50624452095cbffddc5e7337b3602fe2eafbb08
Jasmine Florencio <jasmineflorencio@gmail.com>
2020-08-16 10:28:49 -0700

    move db secrets to config file

--- db.py
+++ db.py
@@ -1,13 +1,17 @@
 from flask import g
 
+import configparser
 import psycopg2
 
 def get_connection():
     if 'db' not in g:
-        g.db = psycopg2.connect(user = "postgres",
-                                password = "hack2020",
-                                host = "34.106.9.169",
-                                port = "5432",
-                                database = "culturefit")
+        config = configparser.ConfigParser()
+        config.read('secrets.ini')
+
+        g.db = psycopg2.connect(user = config['database']['user'],
+                                password = config['database']['password'],
+                                host = config['database']['host'],
+                                port = config['database']['port'],
+                                database = config['database']['database'])
 
     return g.db
--- main.py
+++ main.py
@@ -24,10 +24,13 @@ def get_or_create_company(company_name):
                 company_record = cursor.fetchone()
                 return company_record[0]
 
-def create_user(company_id, position, identity_ids):
-    #create random user name to identify user
+def generate_random_user_name():
     letters = string.ascii_lowercase
-    user_name = ''.join(random.choice(letters) for i in range(8))
+    return ''.join(random.choice(letters) for i in range(8))
+
+def create_user(company_id, position, identity_ids):
+    # create random user name to identify user
+    user_name = generate_random_user_name()
 
     with db.get_connection() as connection:
         with connection.cursor() as cursor:
@@ -43,6 +46,38 @@ def create_user(company_id, position, identity_ids):
 
             return user_id
 
+@app.route('/users')
+def get_users():
+    with db.get_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM users")
+            user_records = cursor.fetchall()
+            column_names = [desc[0] for desc in cursor.description]
+            users = []
+
+            for user_record in user_records:
+                user = dict(zip(column_names, user_record))
+                users.append(user)
+
+            return jsonify(users)
+
+# TODO: check url query params to narrow results
+@app.route('/reviews')
+def get_reviews():
+    with db.get_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM reviews")
+            review_records = cursor.fetchall()
+            column_names = [desc[0] for desc in cursor.description]
+            reviews = []
+
+            # turn db result into key, value pair
+            for review_record in review_records:
+                review = dict(zip(column_names, review_record))
+                reviews.append(review)
+
+            return jsonify(reviews)
+
 '''
 expected json format
 {
@@ -56,11 +91,16 @@ expected json format
 '''
 @app.route('/reviews', methods=["POST"])
 def create_review():
+    # get or create company
     company_name = request.get_json().get("company_name")
     company_id = get_or_create_company(company_name)
+
+    # create a user
     position = request.get_json().get("position")
     identity_ids = request.get_json().get("identities")
     user_id = create_user(company_id, position, identity_ids)
+
+    # create a review
     sentiment = request.get_json().get("sentiment")
     title = request.get_json().get("title")
     body = request.get_json().get("body")
@@ -70,18 +110,10 @@ def create_review():
             cursor.execute(f"INSERT INTO reviews (user_id, sentiment, title, body) VALUES ('{user_id}', '{sentiment}', '{title}', '{body}')")
             connection.commit()
 
-            return jsonify({message: 'Experience submitted'})
+            return jsonify({"message": 'Experience submitted'})
 
 
 
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
-
-'''
-company name
-company experience (neg, neut, pos)
-exp title
-exp body
-identities
-'''
